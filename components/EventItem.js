2b695d18e5de2b8544cc04ae5fbc8a3e87a3b2bc
krishanathep@gmail.com <krishanathep@gmail.com>
2021-12-13 20:29:05 +0700

    include event detail

--- components/EventItem.js
+++ components/EventItem.js
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/styles/EventItem.module.css";
-
 import React from "react";
 
 export default function EventItem({ evt }) {
