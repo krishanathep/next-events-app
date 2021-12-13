7937ca072a529f5b4f7b0c52347fea5357117ec5
krishanathep <krishanathep@gmail.com>
2021-12-13 17:18:11 +0700

    include event details

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
+import { API_URL } from '@/config/index'
+import EventItem from '@/components/EventItem'
 
-export default function Home() {
+export default function Home({events}) {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to Show</h3>}
+
+      {events.map((evt)=>(
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </div>
   )
+}
+
+export async function getStaticProps() {
+  //const res = await fetch('http://localhost:3000/api/events')
+  const res =await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: {events},
+    revalidate: 1
+  }
 }
